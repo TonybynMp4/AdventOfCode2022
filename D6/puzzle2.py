00802e4b9e30ad8243030ad27fb11f5a841ff34e
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-12-06 17:59:52 +0100

    Day 6 puzzle 2

---
+++ D6/puzzle2.py
@@ -0,0 +1,15 @@
+def puzzle1():
+    file = open('D6/input.txt', 'r')
+    Line = file.read()
+
+    for i in range(0, len(Line) - 1):
+        Letters = [Line[i], Line[i+1], Line[i+2], Line[i+3], Line[i+4], Line[i+5], Line[i+6], Line[i+7], Line[i+8], Line[i+9], Line[i+10], Line[i+11], Line[i+12], Line[i+13]]
+        LetterString = ''.join(Letters)
+        check = []
+        for u in LetterString:
+            if LetterString.count(u) == 1:
+                check.append(u)
+        if len(check) == 14:
+            return Line.find(LetterString) + 14
+
+print(puzzle1())
